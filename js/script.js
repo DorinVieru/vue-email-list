a5b0d6d83fb2bdf6259383dab420ab4a084da1a4
DorinVieru <dorino.vieru@gmail.com>
2023-12-20 17:10:36 +0100

    [UP] Create structure of html & js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// DICHIARO VARIABILE DEL METODO createApp
+const { createApp } = Vue;
+
+// INIZIALIZZAZIONE createApp E CREAZIONE ISTANZA DELL'APPLICAZIONE VUE
+createApp({
+    // DEFINISCO IL METODO data () NEL QUALE INSERIRO' UN RETURN
+    data() {
+        return {
+
+        }
+    }, // Chiusura data
+
+    created() {
+
+    }, // Chiusura created
+
+    // DEFINISCO IL METODO methods {} NEL QUALE INSERIRO' LE FUNZIONI
+    methods: {
+
+    }, // Chiusura methods
+
+    // CHIUSURA createApp CON .mount("ID")
+}).mount('#app');
