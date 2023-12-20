2a024611496f9c7188d1ce22fc943cbfed9c0685
DorinVieru <dorino.vieru@gmail.com>
2023-12-20 17:41:45 +0100

    [UP] Complete basic exercise

--- js/script.js
+++ js/script.js
@@ -6,12 +6,17 @@ createApp({
     // DEFINISCO IL METODO data () NEL QUALE INSERIRO' UN RETURN
     data() {
         return {
-
+            mails: [],
         }
     }, // Chiusura data
 
     created() {
-
+        for(let i=0; i<10; i++){
+            // USO LIBRERIA AXIOS PER CONTATTARE L'API
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then( (res) => {
+                this.mails.push(res.data.response);
+            })        
+        }
     }, // Chiusura created
 
     // DEFINISCO IL METODO methods {} NEL QUALE INSERIRO' LE FUNZIONI
